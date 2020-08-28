3d8b272f28f7e2b68c92f5fcee3b53ad02326aab
kiancn <kian.tema@gmail.com>
2020-08-28 10:23:22 +0200

    Version 1

---
+++ src/javan/Jokr.java
@@ -0,0 +1,21 @@
+package javan;
+
+public class Jokr
+{
+    String[] jokes = {
+            "The chicken built the road. Pass along.",
+            "Are you a Rabbi? Please walk into a bar.",
+            "Donkey balls.",
+            "Sane people to not read jokes.",
+            "Catalyst"
+    };
+
+    /**
+     * Method returns a string from jokes String array
+     */
+    public String getRandomJoke()
+    {
+        /* jokes.length does ot need -1 because of int rounding */
+        return jokes[(int)(Math.random() * (jokes.length))];
+    }
+}
---
+++ src/javan/Main.java
@@ -0,0 +1,12 @@
+package javan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* Version 1 */
+        Jokr jokr = new Jokr();
+        System.out.println("Here is your random joke: " + jokr.getRandomJoke());
+
+    }
+}
